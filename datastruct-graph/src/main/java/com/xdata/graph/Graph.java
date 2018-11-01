6f2af199de4df433d75b47556cd9ee5365b49fc7
boyalearn <2114517411@qq.com>
2018-11-01 22:35:33 +0800

    添加图算法

--- datastruct-graph/src/main/java/com/xdata/graph/Graph.java
+++ datastruct-graph/src/main/java/com/xdata/graph/Graph.java
@@ -1,5 +1,17 @@
 package com.xdata.graph;
 
 public class Graph {
+	private Bag<Integer> bag;
+	
+	
+	private class Bag<T>{
+		private T node;
+		private T next;
+	}
+	
+	private class Node<T>{
+		private T data;
+		private T next;
+	}
 
 }
